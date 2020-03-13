dcbaa634d9275e7bbadc1fd494b9177f7236d3da
Ruud Senden <ruud.senden@microfocus.com>
2020-03-13 11:39:31 +0100

    Added script to run Fortify SCA scan

---
+++ fortify-scan.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+projectName=FortifySyncFoDToSSC
+
+# Modular scan doesn't work properly yet, so for now we just add the fortify-client-api build model
+# Note that either approach requires fortify-client-api to be translated/scanned on the same machine
+# before running this script.
+#scanOpts="-scan -include-modules fortify-client-api -Dcom.fortify.sca.UseSynchronousSerialization=true -Dcom.fortify.sca.ThreadCount=1 -Dcom.fortify.sca.DefaultAnalyzers=dataflow"
+scanOpts="-b fortify-client-api -scan" 
+
+set -x
+
+# Remove any old FPR file
+rm -f ${projectName}.fpr
+
+# Clean our build model
+sourceanalyzer -b ${projectName} -clean
+
+# Translate using SCA Gradle integration
+# The -Pfortify option informs our build script that 
+# we're running a Fortify translation, allowing the
+# build script to take appropriate measures if necessary
+sourceanalyzer -b ${projectName} gradle clean build -Pfortify
+
+# Scan the project as a module (SCA 19.2+ feature)
+sourceanalyzer -b ${projectName} -f ${projectName}.fpr ${scanOpts}
