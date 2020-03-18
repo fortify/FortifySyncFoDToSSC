5b9feabe8c4fe66f6dee8b03322eb74df8419d92
rsenden <ruud.senden@microfocus.com>
2020-03-18 10:58:32 +0100

    Updated fortify-scan.sh

--- fortify-scan.sh
+++ fortify-scan.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-projectName=FortifySyncFoDToSSC
-
+# Set scan options
 # Modular scan doesn't work properly yet, so for now we just add the fortify-client-api build model
 # Note that either approach requires fortify-client-api to be translated/scanned on the same machine
 # before running this script.
-#scanOpts="-scan -include-modules fortify-client-api -Dcom.fortify.sca.UseSynchronousSerialization=true -Dcom.fortify.sca.ThreadCount=1 -Dcom.fortify.sca.DefaultAnalyzers=dataflow"
+#scanOpts="-include-modules fortify-client-api -scan"
 scanOpts="-b fortify-client-api -scan" 
 
-set -x
-
-# Clean our build model
-sourceanalyzer -b ${projectName} -clean
-
-# Translate using SCA Gradle integration
-# The -Pfortify option informs our build script that 
-# we're running a Fortify translation, allowing the
-# build script to take appropriate measures if necessary
-sourceanalyzer -b ${projectName} gradle clean build -Pfortify
-
-# Scan the project as a module (SCA 19.2+ feature)
-sourceanalyzer -b ${projectName} -f ${projectName}.fpr ${scanOpts}
+# Load and execute actual scan script from GitHub
+curl -s https://raw.githubusercontent.com/fortify-ps/gradle-helpers/1.0/fortify-scan.sh | bash -s - ${scanOpts}
